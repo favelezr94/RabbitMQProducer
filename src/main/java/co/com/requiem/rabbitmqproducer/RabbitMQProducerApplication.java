ee4bd13b3b1171be904a07b32355cd0eae86e287
Fabian <fabian.velez@nordstrom.com>
2022-07-04 13:24:10 -0500

    Add Readme File.

--- src/main/java/co/com/requiem/rabbitmqproducer/RabbitMQProducerApplication.java
+++ src/main/java/co/com/requiem/rabbitmqproducer/RabbitMQProducerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RabbitMqProducerApplication {
+public class RabbitMQProducerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RabbitMqProducerApplication.class, args);
+        SpringApplication.run(RabbitMQProducerApplication.class, args);
     }
 
 }
